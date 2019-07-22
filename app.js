515207a04d23ffd1fef56b1d4a6a02651046cfc7
Nathan Warren-Acord <nacord26@gmail.com>
2019-07-22 11:58:58 -0700

    Completed.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 const jsonfile = require("jsonfile");
 const dataPath = "./public/json/";
+const faker = require("faker");
 
 app.get("/", function(req, res) {
     var data = jsonfile.readFileSync(dataPath + "index_data.json");
@@ -30,15 +31,47 @@ app.get("/game_ai", function(req, res) {
     res.render("game_ai.html", {data: data});
 });
 
+app.get("/testimonials", function(req, res) {
+    var data = jsonfile.readFileSync(dataPath + "testimonial_data.json");
+    var fourthColor = faker.commerce.color()
+    fourthColor = fourthColor.charAt(0).toUpperCase() + fourthColor.substring(1);
+    var fourthInitials = faker.name.firstName().charAt(0) + ". " + faker.name.firstName().charAt(0) + "."
+    res.render("testimonials.html", {
+        data: data,
+        first: {
+            "area": faker.commerce.department(),
+            "name": faker.fake("{{name.firstName}} {{name.lastName}}, {{name.jobDescriptor}} of {{company.companyName}}")
+        },
+        second: {
+            "adj": faker.company.catchPhraseAdjective().toLowerCase(),
+            "name": faker.fake("{{name.firstName}} {{name.lastName}}, Star of \"The {{random.word}}\"")
+        },
+
+        third: {
+            "buzz-one": faker.company.bsAdjective(),
+            "buzz-two": faker.company.bsAdjective(),
+            "buzz-three": faker.company.bsAdjective(),
+            "name": faker.internet.userName()
+        },
+
+        fourth: {
+            "noun-one": faker.company.catchPhraseDescriptor(),
+            "noun-two": faker.company.catchPhraseDescriptor(),
+            "name": fourthInitials + " " + faker.name.lastName(),
+            "book": "The " + fourthColor + faker.fake(" {{commerce.product}} of {{address.country}}")
+        }
+    });
+});
+
 app.get("/sources", function(req, res) {
     var data = jsonfile.readFileSync(dataPath + "sources_data.json");
     res.render("sources.html", {data: data});
 });
 
-// app.listen("8081", "127.0.0.1", function() {
-//     console.log("Express server is running...");
-// });
-
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("Running Express Server...");
+app.listen("8081", "127.0.0.1", function() {
+    console.log("Express server is running...");
 });
+
+// app.listen(process.env.PORT, process.env.IP, function() {
+//     console.log("Running Express Server...");
+// });
